77d2242034b27207bd09f23919bba2fc1f2f4b17
Marcus <marcus.senise@hotmail.com>
2023-08-07 00:43:48 -0300

    scroll suave transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import initScrollSmooth from './modules/scroll-suave.js';
+import ScrollSmooth from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
@@ -10,7 +10,9 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
-initScrollSmooth();
+const scrollSuave = new ScrollSmooth('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
